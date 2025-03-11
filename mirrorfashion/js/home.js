bbf27735515d659dc4a612bde5b616b763274378
Henrico Birochi <henrico.birochi@gmail.com>
2025-03-11 12:44:43 -0300

    Lista oculta adicionada e os botões para parar e recomeçar o looping das imagens, css avançado adicionado e o css dos botões, javascript dos botões adicionado e para fazer a troca das imagens

--- mirrorfashion/js/home.js
+++ mirrorfashion/js/home.js
@@ -5,3 +5,21 @@ function ValidaBusca(){
     }
 }
 document.querySelector('#form-busca').onsubmit = ValidaBusca;
+
+
+var banners = ["img/destaque-home.png", "img/destaque-home-2.png"];
+var bannerAtual = 0;
+
+function trocaBanner() {
+    bannerAtual = (bannerAtual + 1) % 2;
+    document.querySelector('.destaque img').src = banners[bannerAtual];
+}
+
+var timer = setInterval(trocaBanner, 4000);
+
+function ParaLooping() {
+    clearInterval(timer);
+}
+function RecomecaLooping(){
+    timer = setInterval(trocaBanner, 4000);
+}
